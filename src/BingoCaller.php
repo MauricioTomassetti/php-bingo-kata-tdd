61ae6e254b7917cb108579e05c9a811f7fb95ff8
Mauri88 <mauricio.tomassetti@gmail.com>
2020-12-08 16:47:32 -0300

    First two tests cases (Failure at the beginning)

---
+++ src/BingoCaller.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+class BingoCaller
+{
+
+    private $numbers = [];
+
+    public function __construct()
+    {
+    }
+
+    public function callNumber()
+    {
+        return 0;
+    }
+}
---
+++ tests/BingoCallerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace tests;
+
+use App\BingoCaller;
+use PHPUnit\Framework\TestCase;
+
+class BingoCallerTest extends TestCase
+{
+    public function testWhenCallNumberItsInTheValidRange()
+    {
+        $caller = new BingoCaller();
+        $number = $caller->callNumber();
+
+        //Valdiate assert conditional.
+        $this->assertTrue($number >= 1 && $number <= 75);
+    }
+
+    public function testWhebCalls75TimesAllNumbersArePresent()
+    {
+        $caller = new BingoCaller();
+        $callsNumbers = [];
+        $expectedNumbers = range(1, 75);
+
+        for ($i = 1; $i <= 75; $i++) {
+            $callsNumbers[] = $caller->callNumber();
+        }
+
+        $this->assertEquals($expectedNumbers, $callsNumbers);
+    }
+}
